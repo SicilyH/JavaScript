0b2bdac8223c56fc8a0d846f81ce6a69e27da9cd
Sicily Holguin <weens3@yahoo.com>
2020-11-11 00:06:30 -0700

    js

---
+++ Basic JavaScript Projects/Project1_expressions_alert/JS/main.js
@@ -0,0 +1,15 @@
+window.alert("Hello, World!");
+var A = "<p>This is a string\.</p>"; //Defining a variable and giving it a string
+document.write(A) //Defining a variable
+document.write("<p>Lisa told Bart, \"Knock it off, Bart! Or I'll tell dad!\"<br>\"Eat my shorts!\" Bart responded.</p>"); //Escaping
+document.write("\"Be who you are and say what you feel,"
++ " because those who mind don\'t matter and those who matter don\'t mind.\""
++ "-Dr. Seuss</p>"); //Concatenated string
+var B = "Concatenated" + " String "
+document.write(B)
+var Family = "The Arezzinis", Dad = "Jeremiah", Mom = "Hermoine", Daughter = "Penny", Son = "Zorro";
+document.write(Dad);
+var blues = "</p>I have the blues.";
+var blues = blues.fontcolor("blue"); //Assinging color using fontcolor()method
+document.write(blues);
+document.write(3+3) //Defining an expression
