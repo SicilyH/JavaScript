07f7eed32fdb8817201af9da7c6f8cdcc557a0e3
Sicily Holguin <weens3@yahoo.com>
2020-11-17 02:26:48 -0700

    += operator assingment

--- Basic JavaScript Projects/Project2_functions/JS/main.js
+++ Basic JavaScript Projects/Project2_functions/JS/main.js
@@ -3,6 +3,13 @@ function My_First_Function () {           //Defining a function and naming it
                                           //string value
     var result = str.fontcolor("green");  //Using the fontcolor method on
                                           //str variable
-    document.getElementById("Green_Text") .innerHTML = result;  //Putting the value
+    document.getElementById("Green_Text").innerHTML = result;  //Putting the value
                                         //of result into HTML with "Green_Text" id                                        
+}
+
+function myFunction () {                  //Defining a function and naming it
+    var sentence = "I am learning";       //Defining a variable and using the
+    sentence += " a lot from this book!"; //+= operators to concatenate a string
+    document.getElementById("Concatenate").innerHTML = sentence; //Putting the value
+                                        //of result into HTML with ""
 }
