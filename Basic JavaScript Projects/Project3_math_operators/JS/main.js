0676f721640be1c5135e2d222fba1f79b53eebe5
Sicily Holguin <weens3@yahoo.com>
2020-11-27 11:36:07 -0700

    error comes up

--- Basic JavaScript Projects/Project3_math_operators/JS/main.js
+++ Basic JavaScript Projects/Project3_math_operators/JS/main.js
@@ -1,46 +1,59 @@
-function addition_Function() { //Addition function
+function addition() { //Addition function
     var addition = 2 + 2;
-    document.getElementById("Math").innerHTML = "2 + 2 = " + addition;
+    document.getElementById("Addition").innerHTML = "2 + 2 = " + addition;
 }
 
-function subtraction_Function() { //Subtraction function
+function subtraction() { //Subtraction function
     var Subtraction = 5 - 2;
-    document.getElementById("Math").innerHTML = "5 - 2 = " + Subtraction;
+    document.getElementById("Math1").innerHTML = "5 - 2 = " + Subtraction;
 }
 
 function multiplication() { //Multiplication function
     var simple_Math = 6 * 8;
-    document.getElementById("Math").innerHTML = "6 x 8 = " + simple_Math;
+    document.getElementById("Math2").innerHTML = "6 x 8 = " + simple_Math;
 }
 
 function division() { //Division function
     var simple_Math = 48 / 6;
-    document.getElementById("Math").innerHTML = "48 / 6 = " + simple_Math;
+    document.getElementById("Math3").innerHTML = "48 / 6 = " + simple_Math;
 }
 
 function more_Math () { //Multiple variables
     var simple_Math = (1+2) * 10 / 2 -5;
-    document.getElementById("Math").innerHTML = "1 plus 2, multiplied by 10,
-    divided in half and then subtracted by 5 equals " + simple_Math";
+    document.getElementById("Math4").innerHTML = "1 plus 2, multiplied by 10, divided in half and then subtracted by 5 equals " + simple_Math;
 }
 
 function modulus_Operator() {
     var simple_Math = 25 % 6;
-    document.getElementById("Math").innerHTML = "When you divide 25 by 6 you have a
-    remainder of: " + simple_Math;
+    document.getElementById("Math5").innerHTML = "When you divide 25 by 6 you have a remainder of " + simple_Math;
 }
 
 function negation_Operator() {
     var x = 10;
-    document.getElementById("Math").innerHTML = -x;
+    document.getElementById("Math6").innerHTML = -x;
 }
 
-function var X = 5; //Increment
-X++;
-document.write("X");
+function letter () {
+    X = 5; //Increment
+    X++;
+    document.write("X");
+}
 
-function var Y = 5.25; //Decrement
-Y++;
-document.write(Y);
+function letter () {
+    Y = 5.25; //Decrement
+    Y++;
+    document.write("Y");
+}
 
-window.alert(Math.random()); //Random
+window.alert(Math7.random()); //Random
+
+function my_Dictionary() {
+    var Animal = {
+        Species:"Dog",
+        Color:"Black",
+        Breed:"Labrador",
+        Age:"5",
+        Sound:"Bark!"
+    };
+    document.getElementById("Dictionary").innerHTML = Animal.Sound;
+}
