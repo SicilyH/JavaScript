ad0f283fa2f18a53ad4285d2c6b63c5e90d40c3c
Sicily Holguin <weens3@yahoo.com>
2020-12-01 23:38:25 -0700

    added comments/fixed spacing

--- Basic JavaScript Projects/Project5_comparisons_type_coercion/JS/main.js
+++ Basic JavaScript Projects/Project5_comparisons_type_coercion/JS/main.js
@@ -1,36 +1,35 @@
-document.write(typeof "Word")
-document.write(typeof 3);
+document.write(typeof "Word" + "<br/>") // data type sttring
+document.write(typeof 3 + "<br/>"); //data type number
 
-function my_Function() {
-document.getElementById("Test").innerHTML = 0/0;
+function my_Function() { //NaN= not a number
+document.getElementById("Test1").innerHTML = 0/0;
 }
-
 function my_Function2() {
-document.getElementById("Test").innerHTML = isNaN('This is a string');
+document.getElementById("Test2").innerHTML = isNaN('This is a string'); //NaN string
 }
 
 function my_Function3() {
-    document.getElementById("Test").innerHTML = isNaN('007');
+    document.getElementById("Test3").innerHTML = isNaN('007'); //NaN number
 }
 
-document.write(2E310);
-document.write(-3E310);
-document.write(10 > 2);
-document.write(10 < 2);
-console.log(2 + 2);
-document.write("10" + 5);
-document.write(10 == 10);
-document.write(3 == 11);
+document.write(2E310 + "<br/>"); //infinity
+document.write(-3E310 + "<br/>");//negative infinity
+document.write(10 > 2 + "<br/>" ); //boolean logic
+document.write(10 < 2 + "<br/>");
+console.log(2 + 2 + "<br/>"); // displays 4 in console
+document.write("10" + 5 + "<br/>"); // type coercion
+document.write(10 == 10 + "<br/>"); // double equals, comparison should be made
+document.write(3 == 11 + "<br/>"); // Operators
 
-X = 10;
-Y = 10;
+X = 10; // data on the left side of the symbol is equal to the data on the right side
+Y = 10; // and is same data on the right side
 document.write(X === Y);
 
 X = 82;
 Y = 82;
 document.write(X === Y);
 
-A = "Magnus";
+A = "Magnus"; 
 B = "Magnus";
 document.write(A === B);
 
