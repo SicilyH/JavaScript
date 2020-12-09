fc663e1a2cd72f5b785d23bdf0f4adcffa937359
Sicily Holguin <weens3@yahoo.com>
2020-12-08 22:43:41 -0700

    JS step 129

--- Basic JavaScript Projects/Project6_ternary__operators_constructors/JS/main.js
+++ Basic JavaScript Projects/Project6_ternary__operators_constructors/JS/main.js
@@ -1,11 +1,11 @@
-function Ride_Function() {
+function Ride_Function() {  //Ride
     var Height, Can_ride;
-    Height = document.getElementById("Height).value;
+    Height = document.getElementById("Height").value;
     Can_ride = (Height < 52) ? "You are too short":"You are tall enough";
     document.getElementById("Ride").innerHTML = Can_ride + " to ride.";
 }
 
-function Vehicle(Make, Model, Year, Color) {
+function Vehicle(Make, Model, Year, Color) {  //vehicle make etc.
     this.Vehicle_Make = Make;
     this.Vehicle_Model = Model;
     this.Vehicle_Year = Year;
@@ -15,7 +15,16 @@ var Jack = new Vehicle("Dodge", "Viper", 2020, "Red");
 var Emily = new Vehicle("Jeep", "Trail Hawk", 2019, "White and Black");
 var Erik = new Vehicle("Ford", "Pinto", 1971, "Mustard");
 function New_and_This() {
-    document.getElementById("New_and_This").innerHTML=
-    "Erik drives a " + Erik.Vehicle_Color + "-colored " + Erik.Vehicle_Model + "manufactured in "
-     + Erick.Vehicle_Year;
+    document.getElementById("Keywords_and_Constructors").innerHTML=
+    "Erik drives a " + Erik.Vehicle_Color + "-colored " + Erik.Vehicle_Model + "manufactured in" + Erik.Vehicle_Year;
+}
+
+function Nested_Function () { //nested function/should output 10
+    document.getElementById("Counting").innerHTML = count() ;
+    function count () {
+        var Starting_point = 9;
+        function Plus_one() {Starting_point += 1;}
+        Plus_one();
+        return Starting_point;
+    }
 }
