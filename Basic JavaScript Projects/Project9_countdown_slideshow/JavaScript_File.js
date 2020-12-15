37b080a21212a26ea635944c06111a6398eac219
Sicily Holguin <weens3@yahoo.com>
2020-12-14 21:32:03 -0700

    .

--- Basic JavaScript Projects/Project9_countdown_slideshow/JavaScript_File.js
+++ Basic JavaScript Projects/Project9_countdown_slideshow/JavaScript_File.js
@@ -1,10 +1,11 @@
 // Countdown Function
 function countdown () {
-    var seconds = document.getElementById("seconds").nodeValue;
+    var seconds = document.getElementById("seconds").value;
 
     function tick() { 
         seconds = seconds - 1;
-        TimeRanges.innerHTML = seconds;setTimeout(tick, 1000);
+        document.getElementById("timer").innerHTML = seconds;
+        setTimeout(tick, 1000);
         if(seconds == -1) {
             alert("Time's up!");
         }
