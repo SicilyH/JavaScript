338e8635306b2abe96f6b06db8c48bdbca204546
Sicily Holguin <weens3@yahoo.com>
2020-12-17 21:40:25 -0700

    JS step 205

--- Basic JavaScript Projects/Project10_loops_arrays/main.js
+++ Basic JavaScript Projects/Project10_loops_arrays/main.js
@@ -1,9 +1,37 @@
-function count_To_Ten() {
+function count_To_Ten() {    //while loop
     var Digit = "";
     var X = 1;
     while (X < 11) {
-        Digit += "<br> + X; 
+        Digit += "<br>" + X; 
         X++;
     }
-    document.getElementById("Counting_to_Ten").innerHTML = Digit;
+    document.getElementById("Counting_To_Ten").innerHTML = Digit;
 }
+
+var Instruments = ["Guitar", "Druma", "Piano", "Bass", "Violin", "Trumpet", "Flute"];  //for loop
+var Content = "";
+var Y;
+function for_Loop() {         //for loop
+    for (Y = 0; Y < Instruments.length; Y++) {
+        Content += Instruments[Y] + "<br>";
+    }
+    document.getElementById("List_of_Instruments").innerHTML = Content;
+}
+
+function Cat_Pictures(){     //array
+    var Cat_Picture = [];
+    Cat_Picture[0] = "eating";
+    Cat_Picture[1] = "sleeping";
+    Cat_Picture[2] = "playing";
+    Cat_Picture[3] = "purring";
+    document.getElementById("Cat").innerHTML = "In this picture, the cat is " +
+        Cat_Picture[2] + ".";
+}
+
+var X = 82;         //let
+document.write(X);
+{
+    let X = 33;
+    document.write("<br>" + X);
+}
+document.write("<br>" + X);
