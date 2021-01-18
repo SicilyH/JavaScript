8a510a2abd0adf0824d1374fb8ab34f19d23f55e
Sicily Holguin <weens3@yahoo.com>
2021-01-18 12:04:27 -0800

    JS step 282

--- Basic JavaScript Projects/JavaScripts Projects/Pizza_Project/Pizza_JavaScript/pizza.js
+++ Basic JavaScript Projects/JavaScripts Projects/Pizza_Project/Pizza_JavaScript/pizza.js
@@ -52,7 +52,7 @@ runningTotal = (runningTotal + toppingTotal);
 console.log("total selected topping items: "+toppingCount);
 console.log(toppingCount+" topping - 1 free topping = "+"$"+toppingTotal+".00");
 console.log("topping text1: "+text1);
-console.log(PurchaseTotal:"+"$"+runningTotal+".00);
+console.log("PurchaseTotal:"+"$"+runningTotal+".00");
 document.getElementById("showText").innerHTML=text1;
 document.getElementById("totalPrice").innerHTML = "<h3>Total: <strong>$"+
     runningTotal+".00"+"</strong></h3>"
